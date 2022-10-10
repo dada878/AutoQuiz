0923241809d226f5dac6e43f183bc1e6ff674cc2
冰川MCC <37009584+dada909090@users.noreply.github.com>
2022-10-10 10:20:13 +0800

    支援複選題了

--- read.js
+++ read.js
@@ -2,6 +2,9 @@ var topics = document.querySelectorAll('li[ng-repeat="q in qt.qlist"]');
 var answer = {};
 for (let topic of topics) {
     const description = topic.querySelector('p[ng-bind-html="q.q | trustedHtml"]');
-    const answerOption = topic.querySelector('.rightOption');
-    answer[description.innerHTML] = answerOption.innerHTML;
+    const answerOptions = topic.querySelectorAll('.rightOption');
+    answer[description.innerHTML] = [];
+    for (let opts of answerOptions) {
+        answer[description.innerHTML].push(opts.innerHTML);
+    }
 }
