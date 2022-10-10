fa98cd1a6e6053cfa512e91bbbf87d586f7ff472
冰川MCC <37009584+dada909090@users.noreply.github.com>
2022-10-10 10:19:50 +0800

    支援複選題了

--- load.js
+++ load.js
@@ -3,8 +3,10 @@ for (let topic of topics) {
     const description = topic.querySelector('p[ng-bind-html="q.q | trustedHtml"]');
     const options = topic.querySelectorAll('span[ng-click="qt124Sel(option.value)"]');
     for (let opt of options) {
-        if (answer[description.innerHTML] == opt.innerHTML) {
-            opt.click();
+        for (let ansOpt of answer[description.innerHTML]) {
+            if (ansOpt == opt.innerHTML) {
+                opt.click();
+            }
         }
     }
 }
