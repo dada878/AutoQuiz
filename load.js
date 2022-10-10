17cdec2a09705dea71c33eea6b25d2ac5180dbc0
冰川MCC <37009584+dada909090@users.noreply.github.com>
2022-10-10 10:58:10 +0800

    增加例外情況處理

--- load.js
+++ load.js
@@ -3,10 +3,13 @@ for (let topic of topics) {
     const description = topic.querySelector('p[ng-bind-html="q.q | trustedHtml"]');
     const options = topic.querySelectorAll('span[ng-click="qt124Sel(option.value)"]');
     for (let opt of options) {
-        for (let ansOpt of answer[description.innerHTML]) {
-            if (ansOpt == opt.innerHTML) {
-                opt.click();
+        try {
+            for (let ansOpt of answer[description.innerHTML]) {
+                if (ansOpt == opt.innerHTML) {
+                    opt.click();
+                }
             }
-        }
+        } catch {}
+
     }
 }
