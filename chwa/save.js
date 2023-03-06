9e0381cfaf9f12ac7b147db96cd3d9d5bdd0621e
冰川 <37009584+dada878@users.noreply.github.com>
2023-03-07 01:05:39 +0800

    改成透過 IIFE 執行腳本

--- chwa/save.js
+++ chwa/save.js
@@ -1,12 +1,14 @@
 var answer = {};
-var btns = document.querySelectorAll('.btn:is(.ng-binding):not(.ng-hide):not([id*="sendAns"])');
-var problems = document.querySelectorAll('div[ng-repeat="q in qt.qlist"]:not([style="float: left;"]):is(.ng-scope)');
-for (const pronlem of problems) {
-    const description = pronlem.querySelector('p[ng-bind-html="q.q | trustedHtml"]').innerHTML;
-    const answerOptionElements = pronlem.querySelectorAll('.rightOption');
-    answer[description] = [];
-    for (let opts of answerOptionElements) {
-        answer[description].push(opts.innerHTML);
+(() => {
+    const btns = document.querySelectorAll('.btn:is(.ng-binding):not(.ng-hide):not([id*="sendAns"])');
+    const problems = document.querySelectorAll('div[ng-repeat="q in qt.qlist"]:not([style="float: left;"]):is(.ng-scope)');
+    for (const pronlem of problems) {
+        const description = pronlem.querySelector('p[ng-bind-html="q.q | trustedHtml"]').innerHTML;
+        const answerOptionElements = pronlem.querySelectorAll('.rightOption');
+        answer[description] = [];
+        for (let opts of answerOptionElements) {
+            answer[description].push(opts.innerHTML);
+        }
     }
-}
-alert(`紀錄成功 共 ${Object.keys(answer).length} 題！`);
+    alert(`紀錄成功 共 ${Object.keys(answer).length} 題！`);
+})();
