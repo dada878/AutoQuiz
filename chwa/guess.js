a7646f25dfb710b8a2b0af230bcd3603215dd30d
冰川 <37009584+dada878@users.noreply.github.com>
2023-03-07 01:04:22 +0800

    改成透過 IIFE 執行腳本

--- chwa/guess.js
+++ chwa/guess.js
@@ -1,7 +1,9 @@
-var problems = document.querySelectorAll('div[ng-repeat="q in qt.qlist"]:not([style="float: left;"]):is(.ng-scope)');
-for (let problem of problems) {
-    const description = problem.querySelector('p[ng-bind-html="q.q | trustedHtml"]');
-    const options = problem.querySelectorAll('span[ng-click="qt124Sel(option.value)"]');
-    options[Math.floor(Math.random()*(3-0+1))+0].click();
-}
-for (const btn of document.querySelectorAll('.btn:is(.ng-binding):not(.ng-hide):not([id*="sendAns"])')) btn.click();
+(() => {
+    const problems = document.querySelectorAll('div[ng-repeat="q in qt.qlist"]:not([style="float: left;"]):is(.ng-scope)');
+    for (let problem of problems) {
+        const description = problem.querySelector('p[ng-bind-html="q.q | trustedHtml"]');
+        const options = problem.querySelectorAll('span[ng-click="qt124Sel(option.value)"]');
+        options[Math.floor(Math.random()*(3-0+1))+0].click();
+    }
+    for (const btn of document.querySelectorAll('.btn:is(.ng-binding):not(.ng-hide):not([id*="sendAns"])')) btn.click();
+})();
