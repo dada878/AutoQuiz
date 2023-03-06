62bfa0f42f50e6286f7d8fd75ea8e25f2d63ea62
冰川 <37009584+dada878@users.noreply.github.com>
2023-03-06 23:58:12 +0800

    改版重做 :((

--- chwa/load.js
+++ chwa/load.js
@@ -1,15 +1,14 @@
-var topics = document.querySelectorAll('li[ng-repeat="q in qt.qlist"]');
-for (let topic of topics) {
-    const description = topic.querySelector('p[ng-bind-html="q.q | trustedHtml"]');
-    const options = topic.querySelectorAll('span[ng-click="qt124Sel(option.value)"]');
+var problems = document.querySelectorAll('div[ng-repeat="q in qt.qlist"]:not([style="float: left;"]):is(.ng-scope)');
+for (let problem of problems) {
+    const description = problem.querySelector('p[ng-bind-html="q.q | trustedHtml"]').innerHTML;
+    const options = problem.querySelectorAll('span[ng-click="qt124Sel(option.value)"]');
     for (let opt of options) {
         try {
-            for (let ansOpt of answer[description.innerHTML]) {
+            for (let ansOpt of answer[description]) {
                 if (ansOpt == opt.innerHTML) {
                     opt.click();
                 }
             }
         } catch {}
-
     }
 }
