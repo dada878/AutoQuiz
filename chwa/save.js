a0477909c6606b91ca3a3dfad523c58e1b83049c
冰川 <37009584+dada878@users.noreply.github.com>
2023-03-06 23:55:36 +0800

    改版重做 :(((

--- chwa/save.js
+++ chwa/save.js
@@ -1,10 +1,10 @@
-var topics = document.querySelectorAll('li[ng-repeat="q in qt.qlist"]');
-var answer = {};
-for (let topic of topics) {
-    const description = topic.querySelector('p[ng-bind-html="q.q | trustedHtml"]');
-    const answerOptions = topic.querySelectorAll('.rightOption');
-    answer[description.innerHTML] = [];
-    for (let opts of answerOptions) {
-        answer[description.innerHTML].push(opts.innerHTML);
+var btns = document.querySelectorAll('.btn:is(.ng-binding):not(.ng-hide):not([id*="sendAns"])');
+var problems = document.querySelectorAll('div[ng-repeat="q in qt.qlist"]:not([style="float: left;"]):is(.ng-scope)');
+for (const pronlem of problems) {
+    const description = pronlem.querySelector('p[ng-bind-html="q.q | trustedHtml"]').innerHTML;
+    const answerOptionElements = pronlem.querySelectorAll('.rightOption');
+    answer[description] = [];
+    for (let opts of answerOptionElements) {
+        answer[description].push(opts.innerHTML);
     }
 }
