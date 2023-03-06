6075eca8a01cd07c93431ac8af74fe5f967f02bf
冰川 <37009584+dada878@users.noreply.github.com>
2023-03-07 00:07:23 +0800

    改版重做 :((

--- chwa/guess.js
+++ chwa/guess.js
@@ -1,6 +1,7 @@
-var topics = document.querySelectorAll('li[ng-repeat="q in qt.qlist"]');
-for (let topic of topics) {
-    const description = topic.querySelector('p[ng-bind-html="q.q | trustedHtml"]');
-    const options = topic.querySelectorAll('span[ng-click="qt124Sel(option.value)"]');
+var problems = document.querySelectorAll('div[ng-repeat="q in qt.qlist"]:not([style="float: left;"]):is(.ng-scope)');
+for (let problem of problems) {
+    const description = problem.querySelector('p[ng-bind-html="q.q | trustedHtml"]');
+    const options = problem.querySelectorAll('span[ng-click="qt124Sel(option.value)"]');
     options[Math.floor(Math.random()*(3-0+1))+0].click();
 }
+for (const btn of document.querySelectorAll('.btn:is(.ng-binding):not(.ng-hide):not([id*="sendAns"])')) btn.click();
